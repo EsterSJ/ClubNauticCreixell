dc08353bfb94040eac9dcab51f394c80671330e8
EsterSJ <ester_s@hotmail.com>
2024-02-06 18:03:25 +0100

    models

--- src/app/models/reserva.ts
+++ src/app/models/reserva.ts
@@ -5,13 +5,15 @@ export class Reserva {
     public tipo: string;
     public inicio: Date;
     public fin: Date;
+    public socios: number[];
 
-    constructor(id_reserva: number, id_user: number, tipo: string, inicio: Date, fin: Date){
+    constructor(id_reserva: number, id_user: number, tipo: string, inicio: Date, fin: Date, socios: number[]){
         this.id_reserva = id_reserva;
         this.id_user = id_user;
         this.tipo = tipo;
         this.inicio = inicio;
         this.fin = fin;
+        this.socios = [];
     }
 
 }
